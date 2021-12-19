e0be0a520b0be18c6b540365c4c35ac6aa82aa1b
Shlomi Koncha <cl_shlomikoncha@rnd-hub.com>
2021-12-19 21:10:28 +0200

    fix: deployment

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -3,7 +3,7 @@ export const DEFAULT_SERVER_PORT = 80;
 export const IGNORED_OUTGOING_TRACE_ROUTES = [/^.*\/v1\/metrics.*$/];
 export const IGNORED_INCOMING_TRACE_ROUTES = [/^.*\/docs.*$/];
 
-export const SERVICE_NAME = 'ts-server-boilerplate';
+export const SERVICE_NAME = 'tiles-imploder';
 
 export enum Services {
   LOGGER = 'ILogger',
